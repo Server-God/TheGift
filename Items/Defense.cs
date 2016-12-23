fa86a934af5bebed13da88e579d10debe691c3a0
moseswizz <moses@thevinsons.com>
2016-12-22 19:11:24 -0800

    added Orb of Defense

---
+++ Items/Defense.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+   
+namespace TheGift.Items
+{
+	public class Defense : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Orb of the Defenses";
+			item.width = 40;
+			item.height = 40;
+			item.maxStack = 1;
+			item.toolTip = "Gives so much defense";
+			item.value = 10000000;
+			item.rare = 13;
+			item.defense = 60000;
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+		recipe.AddIngredient(null, "DerpBar", 600);
+		recipe.AddTile(null, "DeathBench");
+		recipe.SetResult(this);
+		recipe.AddRecipe();
+		}
+   }
+}
